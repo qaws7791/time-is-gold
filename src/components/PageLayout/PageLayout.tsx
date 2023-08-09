03ed3d21b2c8aa4fcbac465684ba0efdacd7c9fd
youjunho613 <njn613@gmail.com>
2023-08-09 10:52:52 +0900

    feat. modal 기능 & zustand 적용

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,11 +1,23 @@
+import { Modal } from "components/common";
 import { Outlet } from "react-router-dom";
+import useModalStore from "store/useModalStore";
 import styled from "styled-components";
 
 const PageLayout = () => {
+  const { myProfile, openModal } = useModalStore(state => state);
+
+  const modalOpen = () => openModal("myProfile");
+
   return (
     <FlexBox>
-      <Nav />
-
+      <Nav>
+        <button onClick={modalOpen}>모달열기</button>
+        {myProfile && (
+          <Modal closeTarget="myProfile">
+            <p>test</p>
+          </Modal>
+        )}
+      </Nav>
       <Main>
         <Outlet />
       </Main>
@@ -15,13 +27,17 @@ const PageLayout = () => {
 
 export default PageLayout;
 
+const NAV_SIZE = "360px";
+
 const FlexBox = styled.div`
   display: flex;
 `;
 
 const Nav = styled.nav`
-  width: 360px;
-  height: 100vh;
+  position: fixed;
+
+  width: ${NAV_SIZE};
+  height: 100%;
   border: 1px solid red;
   flex-shrink: 0;
   flex-grow: 0;
@@ -29,4 +45,6 @@ const Nav = styled.nav`
 
 const Main = styled.main`
   width: 100%;
+
+  margin-left: ${NAV_SIZE};
 `;
